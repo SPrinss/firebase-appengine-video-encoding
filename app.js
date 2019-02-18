676b3e0eb1fcdb7d726299472a5c900fe6e17acf
Sam Prinssen <samprinssen@gmail.com>
2019-02-18 12:05:17 +0100

    working copy example from base64

--- app.js
+++ app.js
@@ -3,13 +3,15 @@
 
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const process = require('process'); // Required for mocking environment variables
 const fs = require('fs'); 
-
+const Buffer = require('safe-buffer').Buffer;
 const {Storage} = require('@google-cloud/storage');
 const projectId = 'testing-video-slices';
 const storage = new Storage({projectId: projectId});
 const bucketName = 'testing-video-slices.appspot.com';
+const jsonBodyParser = bodyParser.json();
 
 const app = express();
 app.set('view engine', 'pug');
@@ -20,43 +22,50 @@ app.get('/', (req, res) => {
   res.render('index', {messages: messages});
 });
 
-app.post('/encode/video', async (req, res) => {
+app.post('/encode/video', jsonBodyParser,  async (req, res) => {
   // if (req.query.token !== PUBSUB_VERIFICATION_TOKEN) {
   //   res.status(400).send();
   //   return;
   // }
 
   try {
+    const message = Buffer.from(req.body.message.data, 'base64').toString(
+      'utf-8'
+    );
+
+    var messageObj = JSON.parse(message);
 
     const file = await storage
     .bucket(bucketName)
-    .file('Neo.svg');
+    .file(messageObj.name);
 
-    // Array response with 0 The File metadata, 1 The full API response.
-    const [metaData] = await file.getMetadata();
-    if(metaData.encoded == true) return res.status(200).send();
+    await file.copy('new-copy-buffer-test');
 
-    const newFile = await storage
-    .bucket(bucketName)
-    .file('Neo-copy-2.svg');    
+    // // Array response with 0 The File metadata, 1 The full API response.
+    // const [metaData] = await file.getMetadata();
+    // if(metaData.encoded == true) return res.status(200).send();
+
+    // const newFile = await storage
+    // .bucket(bucketName)
+    // .file('Neo-copy-2.svg');    
     
-    await file.download({destination: '/tmp/tempFile'})
+    // await file.download({destination: '/tmp/tempFile'})
    
-    await newFile.save('tmp/tempFile', {
-      metadata: {
-        contentType: 'image/jpeg',
-        metadata: {
-          custom: 'metadata',
-          encoded: true
-        }
-      },
-        resumable: false
-      });
+    // await newFile.save('tmp/tempFile', {
+    //   metadata: {
+    //     contentType: 'image/jpeg',
+    //     metadata: {
+    //       custom: 'metadata',
+    //       encoded: true
+    //     }
+    //   },
+    //     resumable: false
+    //   });
 
     res.status(200).send();
   
   } catch(error) {
-    res.status(505).send(error);
+    res.status(505).send({error: error});
 
   }
 });
