71e2bb0dd02b7577a98c7237ffa5745eb2444282
Sam Prinssen <samprinssen@gmail.com>
2019-03-04 16:00:15 +0100

    propogate file name & bucket

--- functions/index.js
+++ functions/index.js
@@ -8,7 +8,6 @@ const {PubSub} = require('@google-cloud/pubsub');
 Init
 */
 const projectId = 'testing-video-slices';
-const storageBucket = 'testing-video-slices.appspot.com'; // The Storage bucket that contains the file.
 const pubsub = new PubSub();
 const topicName = 'worker-topic-encode';
 
@@ -16,20 +15,20 @@ const topicName = 'worker-topic-encode';
 Storage Upload Handler
 */
 async function handleFileUpload(object) {
-  // TODO file name
-
   switch(object.contentType) {
     
+    // TODO other media types?
     case 'audio/mp3':
     case 'video/mp4':
       // Must add `type: "new"` the status to the message in order for it to be processed correctly
-      const data = JSON.stringify({ name: 'Neo.svg', bucket: storageBucket, projectId: projectId, status: "new"});
+      const data = JSON.stringify({ name: object.name, bucket: object.bucket, projectId: projectId, status: "new"});
       const dataBuffer = Buffer.from(data);
       const messageId = await pubsub.topic(topicName).publish(dataBuffer);
       console.log(`Published ${messageId}, mime type = ${object.contentType}.`);
       break;
 
     default:
+
       console.info(`Ignoring file with mime type ${object.contentType}.`);
       break;
   }
