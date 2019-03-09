051eff63b8356f9ed9113f343a9ed4f82a059854
Sam Prinssen <samprinssen@gmail.com>
2019-03-09 16:40:54 +0100

    accept all files where MIME type starts with video/audio

--- functions/index.js
+++ functions/index.js
@@ -15,23 +15,16 @@ const topicName = 'worker-topic-encode';
 Storage Upload Handler
 */
 async function handleFileUpload(object) {
-  switch(object.contentType) {
-    
-    // TODO other media types?
-    case 'audio/mp3':
-    case 'video/mp4':
-      // Must add `type: "new"` the status to the message in order for it to be processed correctly
-      const data = JSON.stringify({ name: object.name, bucket: object.bucket, projectId: projectId, status: "new"});
-      const dataBuffer = Buffer.from(data);
-      const messageId = await pubsub.topic(topicName).publish(dataBuffer);
-      console.log(`Published ${messageId}, mime type = ${object.contentType}.`);
-      break;
+  if(object.metadata.encoded == 'true' || object.metadata.encoded == true) return console.info(`File already encoded ${object.contentType}.`);
 
-    default:
-
-      console.info(`Ignoring file with mime type ${object.contentType}.`);
-      break;
+  if(object.contentType.match('audio') || object.contentType.match('video')) {
+    // Must add `type: "new"` the status to the message in order for it to be processed correctly
     const data = JSON.stringify({ name: object.name, bucket: object.bucket, size: object.size, projectId: projectId, status: "new"});
+    const dataBuffer = Buffer.from(data);
+    const messageId = await pubsub.topic(topicName).publish(dataBuffer);
+    console.log(`Published ${messageId}, mime type = ${object.contentType}.`);
+  } else {
+    console.info(`Ignoring file with mime type ${object.contentType}.`);
   }
 }
 
